3c796c3493510005f2033c0beb0dd57e92fd1938
hominhhau <hauxoan333@gmail.com>
2024-08-15 17:52:27 +0700

    bai 62

---
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -0,0 +1,11 @@
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <h2>Sidebar</h2>
+        </div>
+
+    );
+}
+
+export default SideBar;
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,12 @@
+function Profile
+    () {
+    return (
+        <h2>
+            Profile
+
+        </h2>
+    );
+}
+
+export default Profile
+    ;
