2a1720e14db07b7eef62fedf16002dabbe3be491
hominhhau <hauxoan333@gmail.com>
2024-08-16 17:34:31 +0700

    bai 63

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,10 +1,13 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
 
+const cx = classNames.bind(styles);
 function SideBar() {
     return (
-        <div className="sidebar">
+        <aside className={cx('wrapper')}>
             <h2>Sidebar</h2>
-        </div>
 
+        </aside>
     );
 }
 
